f910aced0572af1e734fea433e82768ac27cc0a2
patanrafikhan <iamkhan342@gmail.com>
2023-01-08 20:26:38 +0530

    Added : Head & Tail won Program

---
+++ compute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+read -p "Enter the num1 :" a
+read -p "Enter the num2 :" b
+read -p "Enter the num3 :" c
+
+ranCheck=$(($(($a+$b))*$c))
+
+x=($n1+$n2)
+if($x*$c)
+then
+	echo "Total :"
+fi
