6c522fb8259efdf2d896152f561e0e3f05a72687
patanrafikhan <iamkhan342@gmail.com>
2023-01-08 20:29:37 +0530

    Added : Gambler Program

---
+++ gambler.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+money=100
+win=0
+rounds=0
+while [[ $money -lt 200 && $money -gt 0 ]]
+do
+  reward=$(( RANDOM % 2 ))
+  rounds=$(( $rounds + 1 ))
+  if [ $reward -eq 0 ]
+    then
+      money=$(( $money - 1 ))
+  else
+    money=$(( $money + 1 ))
+    win=$(( $win +1 ))
+  fi
+done
+
+if [ $money -eq 200 ]
+  then
+    echo "He won the gamble"
+else
+  echo "He lost the gamble"
+fi
+echo "No. of wins :" $win
+echo "No of rounds :" $rounds
